e9eb935f7f628ad552ba6adfcbe11daa2d661fe5
Rayla Martins Ferreira <raylamartinsferrreira@gmail.com>
2026-06-11 15:17:36 -0300

    add: link para details

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import apiFilmes from '../../service/apiFilmes';
 import styles from './Card.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Card() {
   const [filmes, setFilmes] = useState([]);
@@ -33,11 +34,12 @@ export default function Card() {
           <h3>{filme.title}</h3>
           
           {filme.poster_path ? (
+            <Link to={`/details/${filme.id}`}>
             <img 
               src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`} 
               alt={filme.title} 
-              className={styles.img} 
-            />
+              className={styles.img} />
+            </Link>
           ) : (
             <div className={styles.semFoto}>Sem Imagem</div>
           )}
