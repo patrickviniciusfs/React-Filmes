a9857bb510bf2ccaaad31758a69c08e03c0881bd
th1agOx <thdarochabp@gmail.com>
2026-06-12 00:30:43 -0300

    Criando componente de feed e consumindo api mockada

--- src/components/ThemeContext/index.jsx
+++ src/components/ThemeContext/index.jsx
@@ -2,15 +2,24 @@ import { createContext, useState, useEffect, useContext } from 'react';
 
 const ThemeContext = createContext();
 
+const LIKE_ACTIVE_COLOR = '#e0245e';
+
+function getInitials(name) {
+  return name ?.split(" ").map((n) => n[0]).join("").slice(0, 2).toUpperCase();
+}
+
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
+
+  const likeColor = theme == 'dark' ? '#ffff' : '#3333';
 
   useEffect(() => {
     const root = window.document.documentElement;
     if (theme === 'dark') {
       root.style.setProperty('--bg', '#121212');
       root.style.setProperty('--text', '#ffffff');
-    } else {
+    }
+    else {
       root.style.setProperty('--bg', '#ffffff');
       root.style.setProperty('--text', '#333333');
     }
@@ -20,7 +29,7 @@ export const ThemeProvider = ({ children }) => {
   const toggleTheme = () => setTheme(p => p === 'light' ? 'dark' : 'light');
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme, toggleTheme, likeColor, likeActiveColor: LIKE_ACTIVE_COLOR, getInitials }}>
       {children}
     </ThemeContext.Provider>
   );
